ffb1838e3458bc451c826014060dc890cc0b70fe
atlas <djlim@rsquare.co.kr>
2022-02-21 18:34:27 +0900

    [init] project

---
+++ OddOrEvenGame_SnapKit/OddOrEvenGame_SnapKit/snapkit/GameView.swift
@@ -0,0 +1,17 @@
+//
+//  GameView.swift
+//  OddOrEvenGame_SnapKit
+//
+//  Created by Atlas on 2022/02/21.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+
+class GameView: UIView {
+    
+    
+    
+}
---
+++ OddOrEvenGame_SnapKit/OddOrEvenGame_SnapKit/snapkit/PlaygroundController.swift
@@ -0,0 +1,16 @@
+//
+//  PlaygroundController.swift
+//  OddOrEvenGame_SnapKit
+//
+//  Created by Atlas on 2022/02/20.
+//
+
+import UIKit
+
+class PlaygroundController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
